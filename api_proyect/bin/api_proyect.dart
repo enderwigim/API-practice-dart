02c170f4e1adc3ef12c348d2556d4ecaf9b57518
Santiago Lorenzano <enderwigim@gmail.com>
2024-07-22 10:06:37 +0200

    add to gitignore

---
+++ api_proyect/bin/api_proyect.dart
@@ -0,0 +1,7 @@
+import 'package:api_proyect/api_proyect.dart' as api_proyect;
+
+void main() {
+  print("Hello world");
+}
+
+
---
+++ api_proyect/test/api_proyect_test.dart
@@ -0,0 +1,8 @@
+import 'package:api_proyect/api_proyect.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
